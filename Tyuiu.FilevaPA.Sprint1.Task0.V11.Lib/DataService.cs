900240e7f54b8aea2b05635f02b109d4b5c83f95
Полина Филeва <polina_fileva@list.ru>
2025-10-27 19:25:31 +0500

    Task 0

--- Tyuiu.FilevaPA.Sprint1.Task0.V11.Lib/DataService.cs
+++ Tyuiu.FilevaPA.Sprint1.Task0.V11.Lib/DataService.cs
@@ -4,7 +4,8 @@ using tyuiu.cources.programming.interfaces.Sprint1;
 
 namespace Tyuiu.FilevaPA.Sprint1.Task0.V11.Lib;
 
-public class DataService : ISprint1Task0V11
+public class DataService : DataService : ISprint1Task0V11
+
 {
     public double Calculate()
     {
